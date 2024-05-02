a25eebddc997d11277c0d61a10bfb6debdb76cd4
la-strole <sovaleto48@gmail.com>
2024-05-02 20:16:53 +0400

    changedocker logging

--- 02run_docker_container.sh
+++ 02run_docker_container.sh
@@ -2,4 +2,13 @@
 
 path=$(pwd)
 
-sudo docker run -v ${path}/database.db:/home/list_parser/database.db -it --env-file ./.env --rm list_parser make --file /home/list_parser/Makefile run
+sudo docker run \
+--name list_am_parser \
+-v ${path}/database.db:/home/list_parser/database.db \
+-d \
+--env-file ./.env \
+--rm \
+--log-driver syslog \
+--log-opt tag=docker/{{.ImageName}} \
+--log-opt syslog-address=unixgram:///dev/log \
+list_parser make --file /home/list_parser/Makefile run
